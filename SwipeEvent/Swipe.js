f607e156fc3a6c8eb7203eb561c918fc2d814cb7
Aaron <aronwebpro@gmail.com>
2018-02-08 21:52:28 -0600

    Swipe event on model scroller

---
+++ SwipeEvent/Swipe.js
@@ -0,0 +1,47 @@
+var Swipe = function () {
+	return {
+		xDown: null,
+		
+		//You need to provide slider, images,next/prev buttons of slider classes 
+		setup: function setup(slider,img,prevBnt,nextBtn) {
+			if(slider === undefined || img === undefined || prevBnt === undefined || nextBtn === undefined) {
+				return;
+			}
+			var _this = this;
+
+			var slider = document.querySelectorAll(slider);
+			slider.forEach(function (e) {
+				var imgs = e.querySelectorAll(img);
+				var prev = e.querySelector(prevBtn);
+				var next = e.querySelector(nextBtn);
+				imgs.forEach(function (el) {
+					el.addEventListener('touchstart', _this.SwipeStart, false);
+					el.addEventListener('touchmove', _this.SwipeMove(next, prev), false);
+				});
+			});
+		},
+		SwipeStart: function SwipeStart(event) {
+			this.xDown = event.touches[0].clientX;
+		},
+		SwipeMove: function SwipeMove(nextClick, preClick) {
+			return function (event) {
+				if (!this.xDown) {
+					return;
+				}
+				var xUp = event.touches[0].clientX;
+				var xDiff = this.xDown - xUp;
+
+				if (xDiff > 0) {
+					nextClick.click();
+				} else {
+					preClick.click();
+				}
+
+				this.xDown = null;
+			};
+		}
+	};
+}();
+
+//To ini
+Swipe.setup();
